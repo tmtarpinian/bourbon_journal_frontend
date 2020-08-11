d649bb6454741d29723473d1abd6e89a0d531d49
ferrisbueller66 <github email address>
2020-08-11 12:20:10 -0500

    add jpg to background, adopt masthead class in css

--- src/index.js
+++ src/index.js
@@ -33,6 +33,21 @@ ReactDOM.render(
                 </div>
             </div>
         </nav>
+
+                {/* -- Masthead -- */}
+        <header class="masthead text-white text-center">
+          <div class="overlay"></div>
+          <div class="container">
+            <div class="row">
+              <div class="col-xl-9 mx-auto">
+                <h1 class="mb-5">BOJO</h1>
+                <h2 class="mb-5">A Sanctuary for the Bourbon Curate</h2>
+
+              </div>
+            </div>
+          </div>
+        </header>
+
         <Route exact path="/bourbons" component={App} />
 
         {/* -- Footer- */}
