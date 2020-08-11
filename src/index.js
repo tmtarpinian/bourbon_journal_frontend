9d40104cffb8fbfb0537855e685b02485f47cf7f
ferrisbueller66 <github email address>
2020-08-11 11:38:30 -0500

    wrote initial css, add header & footer html to index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,65 @@ ReactDOM.render(
   <Provider store={store}> 
     <Router>
       <div>
+      {/* -----------Navigation-------------- */}
+        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                
+                <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/bourbons">Bourbons</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/categories">All Categories</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/bourbons/new">Add a Bourbon</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <Route exact path="/bourbons" component={App} />
+
+        {/* -- Footer- */}
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    {/* -- Footer Location-- */}
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Location</h4>
+                        <p class="lead mb-0">
+                            19 Steve Yzerman Drive
+                            <br />
+                            Detroit, MI 48226
+                        </p>
+                    </div>
+                   {/* -- Footer Social Icons-- */}
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Around the Web</h4>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
+                    </div>
+                    {/* -- Footer About Text-- */}
+                    <div class="col-lg-4">
+                        <h4 class="text-uppercase mb-4">About BoJo</h4>
+                        <p class="lead mb-0">
+                            BoJo is a free to use, MIT licensed React app created by Trevor tarpinian
+                            <br />
+                            <a href="https://github.com/tmtarpinian/bourbon_journal_frontend">Find the Code Here</a>
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        {/* -- Copyright Section-- */}
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright © Trevor Tarpinian 2020</small></div>
+        </div>
+
+
       </div>
     </Router>
   </Provider>,
