71e590449c68b3ec496c53a0143f6556fc1b148b
ferrisbueller66 <github email address>
2020-08-11 21:12:09 -0400

    change a-tags in StaticElements component to NavLinks

--- src/components/StaticElements.js
+++ src/components/StaticElements.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -6,16 +7,16 @@ const Header = () => {
              {/* -----------Navigation-------------- */}
         <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
             <div className="container">
-            <a class="navbar-brand js-scroll-trigger" href="/bourbons">BOJO</a>
+            <Link to="/bourbons" class="navbar-brand js-scroll-trigger" >BOJO</Link>
                 <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/bourbons">Bourbons</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/categories">All Categories</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/bourbons/new">Add a Bourbon</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><Link to="/bourbons" exact class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">Bourbons</Link></li>
+                        <li class="nav-item mx-0 mx-lg-1"><Link to="/categories" exact class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">All Categories</Link></li>
+                        <li class="nav-item mx-0 mx-lg-1"><Link to="/bourbons/new" exact class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">Add a Bourbon</Link></li>
                     </ul>
                 </div>
             </div>
